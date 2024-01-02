94aec5bee314437c0757401c3d894ebb12a0fd47
shakib1729 <shakibahmed0786@gmail.com>
2024-01-02 17:20:34 +0530

    feat: highlight current tab name in NavLinks

--- components/sidebar/NavLinks.tsx
+++ components/sidebar/NavLinks.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 // Libs
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const LINKS = [
   { href: '/chat', label: 'chat' },
@@ -7,13 +10,17 @@ const LINKS = [
   { href: '/tours/new-tour', label: 'new tour' },
   { href: '/profile', label: 'profile' },
 ];
+const HREF_SET = new Set(LINKS.map(link => link.href));
 
 const NavLinks = () => {
+  const pathname = usePathname();
+  const matchedHref = HREF_SET.has(pathname) ? pathname : '/tours';
+
   return (
     <ul className="menu text-base-content gap-1">
       {LINKS.map(link => (
         <li key={link.href}>
-          <Link href={link.href} className="capitalize">
+          <Link href={link.href} className={`capitalize ${matchedHref === link.href ? 'active' : undefined}`}>
             {link.label}
           </Link>
         </li>
